b292d2db18547ae7c4c698eef6370e58a14df093
Jeff <babybluejeff@gmail.com>
2016-09-21 22:39:19 -0700

    Completed lengthOfMissingArray

--- codewars/8kyu/countPositivesSumNegatives.js
+++ codewars/8kyu/countPositivesSumNegatives.js
@@ -1,19 +1,12 @@
 /*
   Description:
+  Given an array of integers, return an array, where the first element is the count
+  of positives numbers and the second element is sum of negative numbers.
 
-  Given an array of integers.
+  If the input array is empty or null, return an empty array.
 
-  Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
-
-  If the input array is empty or null, return an empty array:
-
-  C#/Java: new int[] {} / new int[0];
-  C++: std::vector<int>();
-  JavaScript/CoffeeScript/PHP: [];
-  For example:
-
-  input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
-  return [10, -65].
+  Example:
+  countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]); // [10, -65]
 */
 
 function countPositivesSumNegatives(input) {
