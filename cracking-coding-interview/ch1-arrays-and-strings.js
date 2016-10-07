1b38958207632ddecae3ccb5c0e37733aff6a7c0
Jeff <babybluejeff@gmail.com>
2016-10-07 02:10:26 -0700

    Finished writing questions for ch3

--- cracking-coding-interview/ch1-arrays-and-strings.js
+++ cracking-coding-interview/ch1-arrays-and-strings.js
@@ -124,7 +124,7 @@ isUnique('Jeff'); // false
   Write an algorithm such that if an element in an MxN matrix is 0, its 
   entire row and column are set to 0.
 
-  Hints: #17, #74, #702
+  Hints: #17, #74, #102
 
 ************************************************************************/
 
