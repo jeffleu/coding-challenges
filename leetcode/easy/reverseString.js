0756ad8d11f7033050dfbaf571c2cd3b4e1af3d2
Jeff <babybluejeff@gmail.com>
2016-10-13 01:42:29 -0700

    Completed reverseString

---
+++ leetcode/easy/reverseString.js
@@ -0,0 +1,8 @@
+/*
+  Write a function that takes a string as input and returns the string reversed.
+
+  Example:
+  reverseString('hello'); // 'olleh'
+*/
+
+const reverseString = (s) => s.split('').reverse().join('');
