628b1dce986374f869cfe78c53df2e80bcaf8219
Jeff <babybluejeff@gmail.com>
2016-10-12 18:53:14 -0700

    Finished writing questions for chapter 10

---
+++ cracking-coding-interview/ch10-sorting-and-searching.js
@@ -0,0 +1,10 @@
+/************************************************************************
+  
+  9.8: PASTEBIN
+
+	Design a system like Pastebin, where a user can enter a piece of text
+	and get a randomly generated URL to access it.
+
+	Hints: #165, #184, #206, #232
+
+************************************************************************/
