d58e970a4dd3d4d6f302b4926166c13e123fdf92
Jeffrey Leu - Vendor <vn0z6jc@m-C02L30SRFFT4.local>
2016-11-25 12:32:27 -0800

    Completed 1.7 Rotate Matrix

--- cracking-coding-interview/ch01-arrays-and-strings.js
+++ cracking-coding-interview/ch01-arrays-and-strings.js
@@ -216,7 +216,28 @@ compressString('aab'); // 'aab'
 
 ************************************************************************/
 
+const rotateMatrix = (m) => {
+  let n = m.length;
+  let output = [];
+  
+  for (let c = 0; c < n; c++) {
+    let newRow = [];
+    for (let r = n - 1; r >= 0; r--) {    
+      newRow.push(m[r][c]);
+    }
+    output.push(newRow);
+  }
+  
+  return output;
+};
 
+const matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+
+rotateMatrix(matrix); // [[7,4,1], [8,5,2], [9,6,3]]
 
 /************************************************************************
   
