f899796329c818eb8575bf192b714d50b7f5793f
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-14 20:55:45 -0800

    Updated maxConsecutiveOnes

--- leetcode/easy/maxConsecutiveOnes/maxConsecutiveOnes.js
+++ leetcode/easy/maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findMaxConsecutiveOnes = nums => {
+const findMaxConsecutiveOnes = (nums) => {
   let result = 0;
   let currentCount = 0;
   
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1) currentCount++;
-    if (nums[i + 1] === undefined && currentCount > result) result = currentCount;
-    if (nums[i] === 0) {
-      if (currentCount > result) result = currentCount;
+    if (nums[i] === 1) {
+      currentCount++;
+    } else {
+      result = Math.max(result, currentCount);
       currentCount = 0;
     }
   }
   
-  return result;
+  return Math.max(result, currentCount);
 };
