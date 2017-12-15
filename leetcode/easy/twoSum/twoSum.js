92c997d6dcb95f75bcca6dae21c52f845f584849
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-14 20:42:23 -0800

    Updated twoSum

--- leetcode/easy/twoSum/twoSum.js
+++ leetcode/easy/twoSum/twoSum.js
@@ -4,9 +4,17 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
+  const storage = {};
+  
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+    const diff = target - nums[i];
+    
+    if (!storage.hasOwnProperty(nums[i])) {
+      storage[diff] = i;
+    } else {
+      return [storage[nums[i]], i];
     }
   }
+  
+  return [];
 };
