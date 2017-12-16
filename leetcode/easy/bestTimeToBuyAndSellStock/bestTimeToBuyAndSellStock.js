f6625041fb306be02514e2f4ec0917458d070e8c
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-15 23:12:42 -0800

    Updated bestTimeToBuyAndSellStock

--- leetcode/easy/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.js
+++ leetcode/easy/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.js
@@ -2,17 +2,14 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = prices => {
-  let minPrice = Number.MAX_VALUE;
-  let result = 0;
+const maxProfit = (prices) => {
+  let currentMax = 0;
+  let maxSoFar = 0;
   
-  for (let i = 0; i < prices.length; i++) {
-  	if (prices[i] < minPrice) {
-  		minPrice = prices[i];
-  	} else if (prices[i] - minPrice > result) {
-  		result = prices[i] - minPrice;
-  	}
+  for (let i = 1; i < prices.length; i++) {
+    currentMax = Math.max(0, currentMax += prices[i] - prices[i - 1]);
+    maxSoFar = Math.max(maxSoFar, currentMax);
   }
   
-  return result;
+  return maxSoFar;
 };
