387afd7fe9a8fbdd596af202955bb39dda2c1853
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-19 01:33:57 -0800

    Updated battleshipsInABoard

--- leetcode/medium/battleshipsInABoard/battleshipsInABoard.js
+++ leetcode/medium/battleshipsInABoard/battleshipsInABoard.js
@@ -2,44 +2,25 @@
  * @param {character[][]} board
  * @return {number}
  */
-const addCoordinates = (board, r, c, visited) => {
-  // Check up
-  if (board[r - 1] && board[r - 1][c] === 'X' && !visited.has(`${r - 1} ${c}`)) {
-    visited.add(`${r - 1} ${c}`);
-    addCoordinates(board, r - 1, c, visited);
-  }
-      
-  // Check right
-  if (board[r][c + 1] === 'X' && !visited.has(`${r} ${c + 1}`)) {
-    visited.add(`${r} ${c + 1}`);
-    addCoordinates(board, r, c + 1, visited);
-  }
-      
-  // Check down
-  if (board[r + 1] && board[r + 1][c] === 'X' && !visited.has(`${r + 1} ${c}`)) {
-    visited.add(`${r + 1} ${c}`);
-    addCoordinates(board, r + 1, c, visited);
-  }
-    // Check left
-  if (board[r][c - 1] === 'X' && !visited.has(`${r} ${c - 1}`)) {
-    visited.add(`${r} ${c - 1}`);
-    addCoordinates(board, r, c - 1, visited);
-  }
+const helper = (board, r, c, visited) => {
+  visited.add(`${r} ${c}`);
+  if (board[r - 1] && board[r - 1][c] === 'X' && !visited.has(`${r - 1} ${c}`)) helper(board, r - 1, c, visited);
+  if (board[r + 1] && board[r + 1][c] === 'X' && !visited.has(`${r + 1} ${c}`)) helper(board, r + 1, c, visited);
+  if (board[r][c - 1] === 'X' && !visited.has(`${r} ${c - 1}`)) helper(board, r, c - 1, visited);
+  if (board[r][c + 1] === 'X' && !visited.has(`${r} ${c + 1}`)) helper(board, r, c + 1, visited);
 };
 
-const countBattleships = board => {
+const countBattleships = (board) => {
   const visited = new Set();
-  let result = 0;
   
+  let result = 0;
   for (let r = 0; r < board.length; r++) {
     for (let c = 0; c < board[r].length; c++) {
       if (board[r][c] === 'X' && !visited.has(`${r} ${c}`)) {
-        visited.add(`${r} ${c}`);
+        helper(board, r, c, visited);
         result++;
-        addCoordinates(board, r, c, visited);
       }
     }
   }
-  
   return result;
 };
