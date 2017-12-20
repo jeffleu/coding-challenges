f22d3e03c881c92ae97d48a088679056bb7e1bbe
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-20 15:55:36 -0800

    Updated diameterOfBinaryTree

--- leetcode/easy/diameterOfBinaryTree/diameterofBinaryTree.js
+++ leetcode/easy/diameterOfBinaryTree/diameterofBinaryTree.js
@@ -10,24 +10,15 @@
  * @return {number}
  */
 
-const getDepth = (root) => {
+const getMaxDepth = (root) => {
   if (!root) return 0;
-  return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+  return Math.max(getMaxDepth(root.left), getMaxDepth(root.right)) + 1;
 };
 
 const diameterOfBinaryTree = (root) => {
   if (!root) return 0;
-  let result = 0;
-  const queue = [root];
-  
-  while(queue.length) {
-    const current = queue.shift();
-    const left = getDepth(current.left);
-    const right = getDepth(current.right);
-    if (left + right > result) result = left + right;
-    if (current.left) queue.push(current.left);
-    if (current.right) queue.push(current.right);
-  }
-  
-  return result;
+  const left = diameterOfBinaryTree(root.left);
+  const right = diameterOfBinaryTree(root.right);
+  const self = getMaxDepth(root.left) + getMaxDepth(root.right);
+  return Math.max(left, self, right);
 };
