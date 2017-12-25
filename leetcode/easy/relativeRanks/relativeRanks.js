803535d94e0729f0222cf3170886fe6cf530c8eb
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-25 00:49:00 -0800

    Updated relativeRanks

--- leetcode/easy/relativeRanks/relativeRanks.js
+++ leetcode/easy/relativeRanks/relativeRanks.js
@@ -2,19 +2,21 @@
  * @param {number[]} nums
  * @return {string[]}
  */
-const findRelativeRanks = nums => {
-	const sorted = [...nums].sort((a, b) => b - a);
-	
-	return nums.map(n => {
-		const place = sorted.indexOf(n) + 1;
-		if (place === 1) {
-			return 'Gold Medal';
-		} else if (place === 2) {
-			return 'Silver Medal';
-		} else if (place === 3) {
-			return 'Bronze Medal';
-		} else {
-			return `${place}`;
-		}
-	});
+const findRelativeRanks = (nums) => {
+  const result = [];
+  const sorted = nums.map((num, index) => ({num, index}))
+    .sort((a, b) => b.num - a.num)
+    .forEach((n, i) => {
+      if (i === 0) {
+        result[n.index] = 'Gold Medal';
+      } else if (i === 1) {
+        result[n.index] = 'Silver Medal';
+      } else if (i === 2) {
+        result[n.index] = 'Bronze Medal';
+      } else {
+        result[n.index] = `${i + 1}`;
+      }
+    });
+  
+  return result;
 };
