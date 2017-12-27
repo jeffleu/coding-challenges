f61784cfe48418557d5a761c9eeeb077460cb6fb
Jeff Leu <jeffleu@users.noreply.github.com>
2017-12-27 01:30:03 -0800

    Updated sameTree

--- leetcode/easy/sameTree/sameTree.js
+++ leetcode/easy/sameTree/sameTree.js
@@ -1,37 +1,18 @@
-const isSameTree = (root1, root2) => {
-  if (!root1 && !root2) return true;
-  if (!root1 || !root2) return false;
-  const queue1 = [root1];
-  const queue2 = [root2];
-  let same = true;
-  
-  while(queue1.length || queue2.length) {
-    if (!queue1.length || !queue2.length) {
-      same = false;
-      break;
-    }
-    if (queue1.length && queue2.length) {
-      const current1 = queue1.shift();
-      const current2 = queue2.shift();
-
-      if (current1.val !== current2.val ||
-         (current1.left && !current2.left) || (!current1.left && current2.left) ||
-         (current1.right && !current2.right) || (!current1.right && current2.right)) {
-        same = false;
-        break;
-      }
-      
-      if (current1.left && current2.left) {
-        queue1.push(current1.left);
-        queue2.push(current2.left);
-      }
-      
-      if (current1.right && current2.right) {
-        queue1.push(current1.right);
-        queue2.push(current2.right);
-      }
-    }
-  }
-  
-  return same;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {boolean}
+ */
+const isSameTree = (t1, t2) => {
+  if ((t1 && !t2) || (!t1 && t2)) return false;
+  if (!t1 && !t2) return true;
+  if (t1.val !== t2.val) return false;
+  return isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
 };
