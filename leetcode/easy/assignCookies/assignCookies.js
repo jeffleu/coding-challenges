5d688260d074668acab92fca0d495c3512d4849b
Jeff <jeffreycleu@gmail.com>
2020-05-15 23:45:57 -0700

    Optimized Assign Cookies

--- leetcode/easy/assignCookies/assignCookies.js
+++ leetcode/easy/assignCookies/assignCookies.js
@@ -3,20 +3,26 @@
  * @param {number[]} cookies
  * @return {number}
  */
+
+/*
+Runtime: 92 ms, faster than 46.19% of JavaScript online submissions for Assign Cookies.
+Memory Usage: 37.8 MB, less than 100.00% of JavaScript online submissions for Assign Cookies.
+*/
+
 const findContentChildren = (children, cookies) => {
-  children = children.sort((a, b) => a - b);
-  cookies = cookies.sort((a, b) => a - b);
+  children = children.sort((a, b) => b - a);
+  cookies = cookies.sort((a, b) => b - a);
   let childPointer = 0;
   let cookiePointer = 0;
   let result = 0;
-  
-  while(cookiePointer < cookies.length) {
-    if (children[childPointer] <= cookies[cookiePointer]) {
+  while(childPointer < children.length && cookiePointer < cookies.length) {
+    if (cookies[cookiePointer] >= children[childPointer]) {
       childPointer++;
+      cookiePointer++;
       result++;
+    } else {
+      childPointer++;      
     }
-    cookiePointer++;
   }
-  
   return result;
 };
