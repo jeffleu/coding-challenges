861c241f40bf2ec7a820068ed8a3b0392e093e6d
Jeff <jeffreycleu@gmail.com>
2020-05-19 00:16:48 -0700

    Completed Daily Temperatures

---
+++ leetcode/medium/dailyTemperatures/dailyTemperatures.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} T
+ * @return {number[]}
+ */
+
+/*
+Runtime: 180 ms, faster than 52.78% of JavaScript online submissions for Daily Temperatures.
+Memory Usage: 42.9 MB, less than 44.44% of JavaScript online submissions for Daily Temperatures.
+*/
+
+const dailyTemperatures = T => {
+  const result = [];
+  const stack = [];
+  for (let i = T.length - 1; i >= 0; i--) {
+    if (!stack.length) {
+      result[i] = 0;
+      stack.push(i);
+    } else if (T[i] >= T[stack[stack.length - 1]]) {
+      while(T[i] >= T[stack[stack.length - 1]]) stack.pop();
+      result[i] = !stack.length ? 0 : stack[stack.length - 1] - i;
+      stack.push(i);
+    } else if (T[i] < T[stack[stack.length - 1]]) {
+      result[i] = stack[stack.length - 1] - i;
+      stack.push(i);
+    }
+  }
+  return result;
+};
