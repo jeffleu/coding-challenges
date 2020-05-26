0fb9aa41fa690a32cc4a1b5396d33c78f860fd1c
Jeff <jeffreycleu@gmail.com>
2020-05-26 00:10:46 -0700

    Updated Can Place Flowers

--- leetcode/easy/canPlaceFlowers/canPlaceFlowers.js
+++ leetcode/easy/canPlaceFlowers/canPlaceFlowers.js
@@ -3,15 +3,19 @@
  * @param {number} n
  * @return {boolean}
  */
-const canPlaceFlowers = (flowerbed, n) => {
-  let result = 0;
-  
+
+/*
+Runtime: 68 ms, faster than 60.64% of JavaScript online submissions for Can Place Flowers.
+Memory Usage: 37.4 MB, less than 20.00% of JavaScript online submissions for Can Place Flowers.
+*/
+
+const canPlaceFlowers = function(flowerbed, n) {
   for (let i = 0; i < flowerbed.length; i++) {
-    if (flowerbed[i] === 0 && !flowerbed[i - 1] && !flowerbed[i + 1]) {
-      result++;
+    if (!n) break;
+    if (!flowerbed[i] && !flowerbed[i - 1] && !flowerbed[i + 1]) {
+      n--;
       flowerbed[i] = 1;
     }
   }
-  
-  return n <= result;
+  return !n ? true : false;
 };
